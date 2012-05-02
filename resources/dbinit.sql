d03937304665292a0b75cde5bb5ea5d03120682d
feiyan35488 <feiyan35488@gmail.com>
2012-05-02 16:56:54 +0800

    update build.xml war filename and add comment table

--- resources/dbinit.sql
+++ resources/dbinit.sql
@@ -4,6 +4,8 @@ create table if not exists t_news_tag(news_id int , tag_id int);
 create table if not exists category(id int primary key auto_increment,name varchar(200));
 create table if not exists t_news_category(news_id int ,category_id int);
 
+create table if not exists comment(id int primary key auto_increment,news_id int,username varchar(40),content varchar(200000) ,create_time timestamp);
+
 drop view if exists v_tags;
 create view v_tags as select tag.id as id,tag.name as name,count(news.id) as count from tag,t_news_tag,news where tag.id = tag_id and news.id = news_id group by tag.id;
 drop view if exists v_cats;
