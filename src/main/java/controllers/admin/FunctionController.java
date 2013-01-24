db7435878f73166c1c665a83ea95791e114fe548
tiantian <feiyan35488@gmail.com>
2013-01-24 14:00:34 +0800

    add .java for run in gradle jetty

--- src/main/java/controllers/admin/FunctionController.java
+++ src/main/java/controllers/admin/FunctionController.java
@@ -1,5 +1,8 @@
 package controllers.admin;
 
+import java.io.File;
+import java.net.URISyntaxException;
+
 import javax.servlet.ServletContext;
 
 import org.nutz.dao.Dao;
@@ -12,49 +15,53 @@ import org.nutz.mvc.annotation.Param;
 
 public class FunctionController {
 
-	/**
-	 * 初始化数据库
-	 * @param ctx
-	 * @return
-	 */
-	public String init(ServletContext ctx){
-		
-		 String initSql = ctx.getRealPath("/WEB-INF/classes/dbinit.sql");
-		 Sql sql = Sqls.create("runscript from '"+initSql+"'");
-		 String result = "数据库初始化成功";
-		 try{
-			 dao.execute(sql);
-		 }catch(Exception e){
-			 result="数据库初始化出错了";
-			 log.error(e);
-		 }
-		 return result;
-	}
-	/**
-	 * 执行sql语句，
-	 * @param sql
-	 * @param code
-	 * @return
-	 */
-	public String execsql(@Param("sql")String sql,@Param("code")String code){
-		if(Strings.isEmpty(sql) ){
-			return "请输入验证码和要执行的SQL";
-		}
-		if(Strings.isEmpty(code) || !"tt64".equals(code)){
-			return "验证码输入错误";
-		}
-		 String result = "执行成功";
-		 try{
-			 dao.execute( Sqls.create(sql));
-		 }catch(Exception e){
-			 result="执行出错";
-			 log.error(e);
-		 }
-		 return result;
-	}
-	private static Log log = Logs.get();
-	private Dao dao;
-	public void setDao(Dao dao){
-		this.dao = dao;
-	}
+    /**
+     * 初始化数据库
+     * @param ctx
+     * @return
+     * @throws URISyntaxException 
+     */
+    public String init(ServletContext ctx) throws URISyntaxException {
+
+        String initSql = new File(this.getClass().getResource("/dbinit.sql").toURI()).getAbsolutePath();
+        Sql sql = Sqls.create("runscript from '" + initSql + "'");
+        String result = "数据库初始化成功";
+        try {
+            dao.execute(sql);
+        } catch (Exception e) {
+            result = "数据库初始化出错了";
+            log.error(e);
+        }
+        return result;
+    }
+
+    /**
+     * 执行sql语句，
+     * @param sql
+     * @param code
+     * @return
+     */
+    public String execsql(@Param("sql") String sql, @Param("code") String code) {
+        if (Strings.isEmpty(sql)) {
+            return "请输入验证码和要执行的SQL";
+        }
+        if (Strings.isEmpty(code) || !"tt64".equals(code)) {
+            return "验证码输入错误";
+        }
+        String result = "执行成功";
+        try {
+            dao.execute(Sqls.create(sql));
+        } catch (Exception e) {
+            result = "执行出错";
+            log.error(e);
+        }
+        return result;
+    }
+
+    private static Log log = Logs.get();
+    private Dao dao;
+
+    public void setDao(Dao dao) {
+        this.dao = dao;
+    }
 }
